fa56099057feadf944845d222557ce9fe2b51a6e
vigorski <rollngzhang@gmail.com>
2013-09-10 16:35:43 +0800

    Update tools.js

--- BasicLearn/BasicLearn.Javascript.Part1/Scripts/tools.js
+++ BasicLearn/BasicLearn.Javascript.Part1/Scripts/tools.js
@@ -1,12 +1,19 @@
 function ObjectToString2(OppObj) {
+  if(OppObj.constructor==Array){
+    return '['+OppObj.join(',')+']';
+  }
   if(typeof(OppObj)!="object"){
     return OppObj;
   }
-    var rst = "{";
-    var copy = OppObj;
-    var aPrototype = new Object()
-    for (var member in OppObj) {
-        rst += member + ":" + ObjectToString2(copy[member]) + ',';
-    }
-    return rst.substring(0, rst.length - 1)+'}';
+  var rst = "{";
+  var copy = OppObj;
+  var aPrototype = new Object();
+  for (var member in OppObj) {
+      rst += member + ":" + ObjectToString2(copy[member]) + ',';
+  }
+  return rst.substring(0, rst.length - 1)+'}';
 }
+
+var obj={a:1,b:2,c:{c1:45,c2:43},d:[34,67]};
+var strObj = ObjectToString2(obj);
+var obj2=eval('('+strObj +')');
